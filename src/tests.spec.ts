290c7450899d0e4576babebc1dae2f0e498c4362
IgorBabkin <igba14@gmail.com>
2018-10-27 16:37:28 +0300

    delete index.html

--- src/tests.spec.ts
+++ src/tests.spec.ts
@@ -19,22 +19,4 @@ describe('EvilConsole', () => {
             expect(decorateNumber(15)).to.be.equal('15 FizzBuzz');
         });
     });
-
-    describe('decorateNumbers', () => {
-        it('should print just number', () => {
-            expect(decorateNumber(1)).to.be.equal('1');
-        });
-
-        it('In the case when number is divided by 3 - word Fizz must be added to the number', () => {
-            expect(decorateNumber(3)).to.be.equal('3 Fizz');
-        });
-
-        it('In the case when number is divided by 5 - word Buzz must be added to the number', () => {
-            expect(decorateNumber(5)).to.be.equal('5 Buzz');
-        });
-
-        it('In the case when number is divided by 3 and 5 simultaneously - you need to add the word FizzBuzz to the number', () => {
-            expect(decorateNumber(15)).to.be.equal('15 FizzBuzz');
-        });
-    })
 });
